19780e3dfa745fc3566b62ce443c9e5b1f22ec63
fer2704 <fernandachavezcoronel@gmail.com>
2025-02-21 20:42:49 -0600

    Primer commit

---
+++ config/Migrations/20250222012629_CreateUsuarios.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateUsuarios extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('usuarios');
+        $table->addColumn('nombre', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
